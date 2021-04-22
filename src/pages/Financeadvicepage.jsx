dba6504001e53b94f40dc3f6c12590e94dd5585c
Steven McKeown <stevenmckeown19@gmail.com>
2021-04-23 00:57:24 +0100

    Updated career sectors react cards

--- src/pages/Financeadvicepage.jsx
+++ src/pages/Financeadvicepage.jsx
@@ -31,7 +31,12 @@ const FinanceAdvicePage = () => {
         </div>
       </div>
     </section>
+    
   )
 }
 
+
+
 export default FinanceAdvicePage;
+
+
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -99,62 +99,80 @@ const Homepage = () => {
             </div>
             <CardDeck>
               <Card>
-                <Card.Img variant="top" src="https://matrixrecruitment.ie/wp-content/uploads/2020/01/businessman-working-on-a-project-picture-id1136948339-200x200.jpg" class="rounded-circle" height= "140px" style={{width:"140px"}}/>
+                <Link to = "CovidAdvicePage">
+                <Card.Img variant="top" src="https://wallpaperaccess.com/full/1453916.jpg" />
                 <Card.Body>
                   <Card.Title>Accountancy</Card.Title>
                 </Card.Body>
+                </Link>
               </Card>
               <Card>
-                <Card.Img variant="top" src="https://matrixrecruitment.ie/wp-content/uploads/2021/03/iStock-1221364691-200x200.jpg" />
+              <Link to = "CovidAdvicePage">
+                <Card.Img variant="top" src="https://www.hrmagazine.co.uk/media/fhjgrhus/article-images-2f107217-2fpublic-sector.jpg?anchor=center&mode=crop&width=1002&height=564&bgcolor=White&rnd=132555437236770000" />
                 <Card.Body>
                   <Card.Title>Public Sector</Card.Title>
                 </Card.Body>
+                </Link>
               </Card>
               <Card>
-                <Card.Img variant="top" src="https://matrixrecruitment.ie/wp-content/uploads/2020/01/trading-online-stocks-and-shares-picture-id936338796-200x200.jpg" />
+              <Link to = "CovidAdvicePage">
+                <Card.Img variant="top" src="https://www.nttdata.com/au/en/-/media/nttdataapac/ndau/industries/banking-and-financial-services/banking-and-financial-services-header-2732x1536.jpg" />
                 <Card.Body>
                   <Card.Title>Financial Services</Card.Title>
                 </Card.Body>
+                </Link>
               </Card>
             </CardDeck>
             <CardDeck>
               <Card>
-                <Card.Img variant="top" src="https://matrixrecruitment.ie/wp-content/uploads/2020/01/workers-and-manager-working-with-documents-picture-id1139489459-200x200.jpg" />
+              <Link to = "CovidAdvicePage">
+                <Card.Img variant="top" src="https://d3jh33bzyw1wep.cloudfront.net/s3/W1siZiIsIjIwMTkvMTEvMjEvMTIvMDYvMjcvMzIvc2h1dHRlcnN0b2NrXzE0OTg3NDI1MTkuanBnIl0sWyJwIiwidGh1bWIiLCI4MDB4NDUwIyJdXQ" />
                 <Card.Body>
                   <Card.Title>Engineering</Card.Title>
                 </Card.Body>
+                </Link>
               </Card>
               <Card>
-                <Card.Img variant="top" src="https://matrixrecruitment.ie/wp-content/uploads/2020/01/gears-or-cogwheels-picture-id904315536-200x200.jpg" />
+              <Link to = "CovidAdvicePage">
+                <Card.Img variant="top" src="https://www.jpmorgan.com/content/dam/jpm/commercial-banking/insights/market-and-economy/callout-highs-lows-of-manufacturings-recovery.jpg" />
                 <Card.Body>
                   <Card.Title>Manufacturing</Card.Title>
                 </Card.Body>
+                </Link>
               </Card>
               <Card>
-                <Card.Img variant="top" src="https://matrixrecruitment.ie/wp-content/uploads/2020/01/researching-equipment-picture-id471521214-200x200.jpg" />
+              <Link to = "CovidAdvicePage">
+                <Card.Img variant="top" src="https://www.pharmaceutical-technology.com/wp-content/uploads/sites/10/2020/10/Feature-image-top-ten-pharma-companies-in-2020.jpg" />
                 <Card.Body>
                   <Card.Title>Quality and Laboratory</Card.Title>
                 </Card.Body>
+                </Link>
               </Card>
             </CardDeck>
             <CardDeck>
               <Card>
-                <Card.Img variant="top" src="https://matrixrecruitment.ie/wp-content/uploads/2020/01/stack-of-blue-containers-box-cargo-freight-ship-for-import-export-3d-picture-id1162163400-200x200.jpg" />
+              <Link to = "CovidAdvicePage">
+                <Card.Img variant="top" src="https://miuc.org/wp-content/uploads/2019/11/Supply-Chain-Management.jpg" />
                 <Card.Body>
                   <Card.Title>Supply Chain</Card.Title>
                 </Card.Body>
+                </Link>
               </Card>
               <Card>
-                <Card.Img variant="top" src="https://matrixrecruitment.ie/wp-content/uploads/2020/01/aerial-view-of-supported-hands-out-together-picture-id866059338-200x200.jpg" />
+              <Link to = "CovidAdvicePage">
+                <Card.Img variant="top" src="https://cdn.lynda.com/course/704123/704123-637491210983328614-16x9.jpg" />
                 <Card.Body>
                   <Card.Title>Human Resources</Card.Title>
                 </Card.Body>
+                </Link>
               </Card>
               <Card>
-                <Card.Img variant="top" src="https://matrixrecruitment.ie/wp-content/uploads/2021/02/Contracting-Matrix-Recruitment-350x200.jpg" />
+              <Link to = "CovidAdvicePage">
+                <Card.Img variant="top" src="https://www.freestudy.com/wp-content/uploads/2019/08/Best-Free-Online-Information-Technology-Courses.jpg" />
                 <Card.Body>
                   <Card.Title>IT</Card.Title>
                 </Card.Body>
+                </Link>
               </Card>
             </CardDeck>
           </div>
@@ -169,7 +187,6 @@ const Homepage = () => {
               </div>
             </div>
             <CardDeck >
-            
               <Card style={{ width: '20rem', height: '18rem' }}>
               <Link to = "CovidAdvicePage">
                 <Card.Img variant="top" src="https://matrixrecruitment.ie/wp-content/uploads/2021/03/Copy-of-Dream-job-survey-image-350x200.jpg" />
@@ -178,8 +195,7 @@ const Homepage = () => {
                 </Card.Body>
                 </Link>
               </Card>
-           
-           
+             
               <Card style={{ width: '20rem', height: '18rem' }}>
               <Link to = "FinanceAdvicePage">
                 <Card.Img variant="top" src="https://matrixrecruitment.ie/wp-content/uploads/2021/02/iStock-468044048-350x200.jpg" />
@@ -189,7 +205,6 @@ const Homepage = () => {
                 </Link>
               </Card>
             
-            
               <Card style={{width: '20rem', height: '18rem' }}>
               <Link to = "ContractingAdvicePage">
                 <Card.Img variant="top" src="https://matrixrecruitment.ie/wp-content/uploads/2021/02/Contracting-Matrix-Recruitment-350x200.jpg" />
@@ -197,8 +212,7 @@ const Homepage = () => {
                   <Card.Title>Why Contracting Roles are Overtaking Permanent Positions</Card.Title>
                 </Card.Body>
                 </Link>
-              </Card>
-             
+              </Card>         
             </CardDeck>
           </div>
         </section>
