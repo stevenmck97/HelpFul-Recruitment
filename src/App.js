536a20e1c963d6d49dcbc1ed3e163c07a8e8881e
Steven McKeown <stevenmckeown19@gmail.com>
2021-04-22 23:32:48 +0100

    Removed unused imports and files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
 import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
-// import logo from './logo.svg';
-import Portfolio from './components/Portfolio'
 import './App.css';
 import './assets/css/bootstrap.min.css'
-import Card from 'react-bootstrap/Card'
-import CardDeck from 'react-bootstrap/CardDeck'
 import Homepage from './pages/Homepage'
-// import Aboutpage from './pages/Aboutpage'
-// import Advicepage from './pages/Advicepage'
-// import Careerspage from './pages/Careerspage'
 import CovidAdvicePage from './pages/Covidadvicepage'
 import FinanceAdvicePage from './pages/Financeadvicepage'
 import ContractingAdvicePage from './pages/Contractingadvicepage'
 import SiteHeader from './components/Siteheader'
 
-// import advicepage1 from './pages/advicepage1'
+
 
 function App() {
   
@@ -24,9 +17,6 @@ function App() {
     <SiteHeader/>
     <Switch> 
       <Route exact path = "/" component={Homepage}/>
-      {/* <Route exact path = "/Advicepage" component={Advicepage}/> 
-      <Route exact path = "/Careerspage" component={Careerspage}/> 
-      <Route exact path = "/Aboutpage" component={Aboutpage}/>  */}
       <Route exact path = "/CovidAdvicePage" component={CovidAdvicePage}/>   
       <Route exact path = "/FinanceAdvicePage" component={FinanceAdvicePage}/> 
       <Route exact path = "/ContractingAdvicePage" component={ContractingAdvicePage}/>
