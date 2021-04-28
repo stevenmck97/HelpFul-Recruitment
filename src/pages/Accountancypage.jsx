d42b566f27b9f4e792bec4c22049a03cfafd3f63
Steven McKeown <stevenmckeown19@gmail.com>
2021-04-28 03:06:54 +0100

    Updated accountancy page with more jobs on different websites

--- src/pages/Accountancypage.jsx
+++ src/pages/Accountancypage.jsx
@@ -17,6 +17,7 @@ const AccountancyPage = () => {
             <p></p>
             <Table striped bordered hover>
               <thead>
+              <h3>Matrix Recruitment</h3>
                 <tr>
                   <th>Job Title</th>
                   <th>Company</th>
@@ -26,10 +27,42 @@ const AccountancyPage = () => {
               </thead>
               <tbody>
                 <tr>
-                  <td>Accounts Payable - Contract</td>
-                  <td>Mark</td>
+                  <td>Accounts Payable</td>
+                  <td>Not Disclosed</td>
                   <td>Wicklow, Carlow, Kildare</td>
-                  <td><Button variant="primary" href="https://matrixrecruitment.ie/job/accounts-payable-contract-916972/" target="_blank">Matrix Recruitment</Button></td>
+                  <td><Button variant="primary" href="https://matrixrecruitment.ie/job/accounts-payable-contract-916972/" target="_blank">Click here to apply</Button></td>
+                </tr>
+                <tr>
+                  <td>Financial Accountant</td>
+                  <td>Not Disclosed</td>
+                  <td>Galway, Laois, Meath, Offaly, Westmeath</td>
+                  <td><Button variant="primary" href="https://matrixrecruitment.ie/job/financial-accountant-immediate-start-916700/" target="_blank">Click here to apply</Button></td>
+                </tr>
+                <tr>
+                  <td>Accounts Assistant</td>
+                  <td>Not Disclosed</td>
+                  <td>Offaly</td>
+                  <td><Button variant="primary" href="https://matrixrecruitment.ie/job/accounts-payable-contract-916972/" target="_blank">Click here to apply</Button></td>
+                </tr>
+               <br></br>
+                <h3>Indeed</h3>
+                <tr>
+                  <td>Client Services Executive</td>
+                  <td>Accountant Online</td>
+                  <td>Remote</td>
+                  <td><Button variant="primary" href="https://ie.indeed.com/jobs?q=accountancy&l&advn=9942618280296645&vjk=5e9a1724f1f84832" target="_blank">Click here to apply</Button></td>
+                </tr>
+                <tr>
+                  <td>Bookkeeper / Accounts Assistant - Part Time</td>
+                  <td>Sound Accounts Limited</td>
+                  <td>Dublin</td>
+                  <td><Button variant="primary" href="https://ie.indeed.com/jobs?q=accountancy&l&advn=7961159670409192&vjk=8647902d80b7f8e1" target="_blank">Click here to apply</Button></td>
+                </tr>
+                <tr>
+                  <td>Trainee Accountant</td>
+                  <td>Zurich Insurance</td>
+                  <td>Dublin</td>
+                  <td><Button variant="primary" href="https://ie.indeed.com/jobs?q=accountancy&l&vjk=ac80c689a8cdabcb" target="_blank">Click here to apply</Button></td>
                 </tr>
               </tbody>
             </Table>
