92a0da67748b21f40ef4fde2f3b5814e6de02f55
Steven McKeown <stevenmckeown19@gmail.com>
2021-04-28 02:07:09 +0100

    added useful links section to the footer

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -186,16 +186,43 @@ const Homepage = () => {
           </div>
         </section>
 
+        <section className="bg-light page-section">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-12 text-center">
+                <h2 className="section-heading text-uppercase">Recommended Videos</h2>
+              </div>
+            </div>
+            <div className="row">
+            <iframe width="1280" height="720" src="https://www.youtube.com/embed/acijNEErf-c" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+            </div>
+          </div>
+        </section>
+
         <footer className="footer">
           <div className="container">
             <div className="row align-items-center">
               <div className="col-md-4">
                 <span className="copyright">Developed by Steven McKeown</span>
+              
               </div>
               <div className="col-md-4">
+                <h4>Useful Links</h4>
                 <ul className="list-inline quicklinks">
-                  <li className="list-inline-item">
-                    <a href="#something">Disclaimer</a>
+                  <li>
+                    <a href="https://www.linkedin.com/jobs/search/?f_C=1337%2C2587638%2C39939%2C290903%2C9202023%2C2561065&geoId=104738515&location=Ireland" target="_blank" rel="noreferrer">LinkedIn</a>
+                  </li>
+                  <li>
+                    <a href="https://ie.indeed.com/" target="_blank" rel="noreferrer">Indeed.ie</a>
+                  </li>
+                  <li>
+                    <a href="https://www.jobs.ie/" target="_blank" rel="noreferrer">Jobs.ie</a>
+                  </li>
+                  <li>
+                    <a href="https://www.irishjobs.ie/" target="_blank" rel="noreferrer">Irish Jobs</a>
+                  </li>
+                  <li>
+                    <a href="https://gradireland.com/" target="_blank" rel="noreferrer">GradIreland</a>
                   </li>
                 </ul>
               </div>
