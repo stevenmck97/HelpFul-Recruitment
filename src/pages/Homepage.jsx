ba8edb131ca040bb480db79d57a0a8e3fe6040b0
Steven McKeown <stevenmckeown19@gmail.com>
2021-04-28 01:15:43 +0100

    Removed unused and commented out code in homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -14,7 +14,7 @@ const Homepage = () => {
             <div className="intro-text">
               <div className="intro-lead-in">Welcome To Helpful Recruitment!</div>
               <div className="intro-heading text-uppercase">Placeholder</div>
-              <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#services">About Us</a>
+              <a className="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#about">About Us</a>
             </div>
           </div>
         </header>
@@ -186,204 +186,17 @@ const Homepage = () => {
           </div>
         </section>
 
-        
-        {/* <section className="bg-light page-section" id="team">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12 text-center">
-                <h2 className="section-heading text-uppercase">Our Amazing Team</h2>
-                <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-sm-4">
-                <div className="team-member">
-                  <img className="mx-auto rounded-circle" src="img/team/1.jpg" alt="" />
-                  <h4>Kay Garland</h4>
-                  <p className="text-muted">Lead Designer</p>
-                  <ul className="list-inline social-buttons">
-                    <li className="list-inline-item">
-                      <a href="#something">
-                        <i className="fa fa-twitter"></i>
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a href="#something">
-                        <i className="fa fa-facebook-f"></i>
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a href="#something">
-                        <i className="fa fa-linkedin-in"></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-sm-4">
-                <div className="team-member">
-                  <img className="mx-auto rounded-circle" src="img/team/2.jpg" alt="" />
-                  <h4>Larry Parker</h4>
-                  <p className="text-muted">Lead Marketer</p>
-                  <ul className="list-inline social-buttons">
-                    <li className="list-inline-item">
-                      <a href="#something">
-                        <i className="fa fa-twitter"></i>
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a href="#something">
-                        <i className="fa fa-facebook-f"></i>
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a href="#something">
-                        <i className="fa fa-linkedin-in"></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-              <div className="col-sm-4">
-                <div className="team-member">
-                  <img className="mx-auto rounded-circle" src="img/team/3.jpg" alt="" />
-                  <h4>Diana Pertersen</h4>
-                  <p className="text-muted">Lead Developer</p>
-                  <ul className="list-inline social-buttons">
-                    <li className="list-inline-item">
-                      <a href="#something">
-                        <i className="fa fa-twitter"></i>
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a href="#something">
-                        <i className="fa fa-facebook-f"></i>
-                      </a>
-                    </li>
-                    <li className="list-inline-item">
-                      <a href="#something">
-                        <i className="fa fa-linkedin-in"></i>
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-lg-8 mx-auto text-center">
-                <p className="large text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut eaque, laboriosam veritatis, quos non quis ad perspiciatis, totam corporis ea, alias ut unde.</p>
-              </div>
-            </div>
-          </div>
-        </section>
-  
-  
-        <section className="py-5">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-3 col-sm-6">
-                <a href="#something">
-                  <img className="img-fluid d-block mx-auto" src="img/logos/envato.jpg" alt="" />
-                </a>
-              </div>
-              <div className="col-md-3 col-sm-6">
-                <a href="#something">
-                  <img className="img-fluid d-block mx-auto" src="img/logos/designmodo.jpg" alt="" />
-                </a>
-              </div>
-              <div className="col-md-3 col-sm-6">
-                <a href="#something">
-                  <img className="img-fluid d-block mx-auto" src="img/logos/themeforest.jpg" alt="" />
-                </a>
-              </div>
-              <div className="col-md-3 col-sm-6">
-                <a href="#something">
-                  <img className="img-fluid d-block mx-auto" src="img/logos/creative-market.jpg" alt="" />
-                </a>
-              </div>
-            </div>
-          </div>
-        </section>
-  
-  
-        <section className="page-section" id="contact">
-          <div className="container">
-            <div className="row">
-              <div className="col-lg-12 text-center">
-                <h2 className="section-heading text-uppercase">Contact Us</h2>
-                <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
-              </div>
-            </div>
-            <div className="row">
-              <div className="col-lg-12">
-                <form id="contactForm" name="sentMessage" novalidate="novalidate">
-                  <div className="row">
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <input className="form-control" id="name" type="text" placeholder="Your Name *" required="required" data-validation-required-message="Please enter your name." />
-                        <p className="help-block text-danger"></p>
-                      </div>
-                      <div className="form-group">
-                        <input className="form-control" id="email" type="email" placeholder="Your Email *" required="required" data-validation-required-message="Please enter your email address." />
-                        <p className="help-block text-danger"></p>
-                      </div>
-                      <div className="form-group">
-                        <input className="form-control" id="phone" type="tel" placeholder="Your Phone *" required="required" data-validation-required-message="Please enter your phone number." />
-                        <p className="help-block text-danger"></p>
-                      </div>
-                    </div>
-                    <div className="col-md-6">
-                      <div className="form-group">
-                        <textarea className="form-control" id="message" placeholder="Your Message *" required="required" data-validation-required-message="Please enter a message."></textarea>
-                        <p className="help-block text-danger"></p>
-                      </div>
-                    </div>
-                    <div className="clearfix"></div>
-                    <div className="col-lg-12 text-center">
-                      <div id="success"></div>
-                      <button id="sendMessageButton" className="btn btn-primary btn-xl text-uppercase" type="submit">Send Message</button>
-                    </div>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </section> */}
-  
-  
         <footer className="footer">
           <div className="container">
             <div className="row align-items-center">
               <div className="col-md-4">
                 <span className="copyright">Developed by Steven McKeown</span>
               </div>
-              {/* <div className="col-md-4">
-                <ul className="list-inline social-buttons">
-                  <li className="list-inline-item">
-                    <a href="#something">
-                      <i className="fa fa-twitter"></i>
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a href="#something">
-                      <i className="fa fa-facebook-f"></i>
-                    </a>
-                  </li>
-                  <li className="list-inline-item">
-                    <a href="#something">
-                      <i className="fa fa-linkedin-in"></i>
-                    </a>
-                  </li>
-                </ul>
-              </div> */}
               <div className="col-md-4">
                 <ul className="list-inline quicklinks">
                   <li className="list-inline-item">
                     <a href="#something">Disclaimer</a>
                   </li>
-                  {/* <li className="list-inline-item">
-                    <a href="#something">Terms of Use</a>
-                  </li> */}
                 </ul>
               </div>
             </div>
